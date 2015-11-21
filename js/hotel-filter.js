b2ad253f0d0a508e91ee8542be5f3471d0dd76b6
Pavel <sergeevps@gmail.com>
2015-11-21 14:01:12 +0500

    fix hotel-filter.js

--- js/hotel-filter.js
+++ js/hotel-filter.js
@@ -1,10 +1,9 @@
 var form = document.querySelector(".search-form");
 var btnShow = document.querySelector(".btn-show");
 var btnShowHide = document.querySelector(".btn-show-hide");
-//if (!document.querySelector("a.btn-show")) {
+//replace link to button
 btnShow.classList.remove("btn-show");
 btnShow.classList.add("btn-show-hide");
-
 btnShowHide.classList.remove("btn-show-hide");
 btnShowHide.classList.add("btn-show");
 btnShowHide.classList.add("btn");
