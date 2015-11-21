e203f4c6939639fd81bb8a51fe840b2713412374
Pavel <sergeevps@gmail.com>
2015-11-21 23:09:13 +0500

    minificator

--- js/hotel-filter.js
+++
@@ -1,9 +0,0 @@
-var form = document.querySelector(".search-form");
-var btnShow = document.querySelector(".btn-show");
-var btnShowHide = document.querySelector(".btn-show-hide");
-//replace link to button
-btnShow.classList.remove("btn-show");
-btnShow.classList.add("btn-show-hide");
-btnShowHide.classList.remove("btn-show-hide");
-btnShowHide.classList.add("btn-show");
-btnShowHide.classList.add("btn");
