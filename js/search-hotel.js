436ba67866c63bcee4a8173b13de8c7c2de27861
Pavel <sergeevps@gmail.com>
2015-11-21 13:49:46 +0500

    add map

--- js/search-hotel.js
+++ js/search-hotel.js
@@ -1,14 +1,17 @@
 var link = document.querySelector(".link-to-btn");
 var popup = document.querySelector(".search-hotel-form");
 var arrival = popup.querySelector("[name=arrival-date]");
+var map = document.querySelector(".map");
 
 link.addEventListener("click", function(event) {
   if (!popup.classList.contains("search-hotel-form-show")) {
     event.preventDefault();
+    map.classList.add("map-hide");
     popup.classList.add("search-hotel-form-show");
     arrival.focus();
   } else {
     event.preventDefault();
     popup.classList.remove("search-hotel-form-show");
+    map.classList.remove("map-hide");
   }
 });
