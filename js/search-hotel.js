5f625bd1bda79e6011f9707fd79c640eab2c862c
Pavel <sergeevps@gmail.com>
2015-11-21 22:17:18 +0500

    search-form js fix

--- js/search-hotel.js
+++ js/search-hotel.js
@@ -1,6 +1,9 @@
 var link = document.querySelector(".link-to-btn");
 var popup = document.querySelector(".search-hotel-form");
+var form = document.querySelector(".search-form");
 var arrival = popup.querySelector("[name=arrival-date]");
+var departure = popup.querySelector("[name=departure-date]");
+var adults = popup.querySelector("[name=adults]");
 var map = document.querySelector(".map");
 
 link.addEventListener("click", function(event) {
@@ -13,5 +16,14 @@ link.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.remove("search-hotel-form-show");
     map.classList.remove("map-hide");
+  }
+});
+
+form.addEventListener("submit", function(event) {
+  if (!arrival.value || !departure.value || !(adults.value > 0)) {
+      event.preventDefault();
+      popup.classList.remove("search-hotel-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("search-hotel-error");
   }
 });
